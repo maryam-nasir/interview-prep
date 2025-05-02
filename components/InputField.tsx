87619e2354f7f2700c7125488a0c42eb5d8e9921
Maryam Nasir <maryamnasir555@yahoo.com>
2025-05-02 11:08:21 +0500

    Add Input component

---
+++ components/InputField.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { FormControl, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+
+type InputFieldProps<T extends FieldValues> = {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+};
+
+const InputField = ({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: InputFieldProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input
+              className="input"
+              placeholder={placeholder}
+              type={type}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default InputField;
