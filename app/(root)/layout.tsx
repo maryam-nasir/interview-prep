b1359cf26a749105dc64722f223ec2989a41ce04
Maryam Nasir <maryamnasir555@yahoo.com>
2025-05-02 17:38:17 +0500

    Add navigation bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const RootLayout = ({children}: Readonly<{children: React.ReactNode}>) => {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <div>{children}</div>
-  )
-}
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
 
-export default RootLayout
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
+  );
+};
+
+export default RootLayout;
